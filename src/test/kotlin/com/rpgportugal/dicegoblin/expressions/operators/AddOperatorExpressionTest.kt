0fd9985c6c3d9ec30fa318347852cc14d032dc6a
Adriano Anastácio <adriano.halo@gmail.com>
2025-01-10 00:45:39 +0000

    Cleanup

--- src/test/kotlin/com/rpgportugal/dicegoblin/expressions/operators/AddOperatorExpressionTest.kt
+++ src/test/kotlin/com/rpgportugal/dicegoblin/expressions/operators/AddOperatorExpressionTest.kt
@@ -10,31 +10,32 @@ class AddOperatorExpressionTest {
 
     @Test
     fun `Tests adding two statics`() {
-        val staticLeft = Expression.Number(4)
-        val staticRight = Expression.Number(10)
+        val lhs = Expression.Number(4)
+        val rhs = Expression.Number(10)
 
-        val addOp = Expression.Add(staticLeft, staticRight)
+        val add = Expression.Add(lhs, rhs)
 
-        assertEquals(14, addOp.evaluate { 0 }.value)
+        assertEquals(14, add.evaluate { 0 }.value)
     }
 
     @Test
     fun `Tests adding a NumberDice and a static`() {
-        val l = Expression.Dice(Dice.numeric(3))
-        val r = Expression.Number(1)
+        val lhs = Expression.Dice(Dice.numeric(3))
+        val rhs = Expression.Number(1)
 
-        val addExpr = Expression.Add(l, r)
+        val add = Expression.Add(lhs, rhs)
 
-        assertEquals(Face.Dice(4,"3"), addExpr.evaluate { 2 })
+        assertEquals(Face.Dice(4,"3"), add.evaluate { 2 })
     }
 
     @Test
     fun `Tests if the expressionResults match the final result`() {
-        val staticLeft = Expression.Dice(Dice.numeric(2))
-        val staticRight = Expression.Number(1)
+        val lhs = Expression.Dice(Dice.numeric(2))
+        val rhs = Expression.Number(1)
 
-        val addExpr  = Expression.Add(staticLeft, staticRight)
-        val result = addExpr.evaluate { 1 }
+        val add  = Expression.Add(lhs, rhs)
+
+        val result = add.evaluate { 1 }
 
         assertEquals(Face.Dice(3, "2"), result)
     }
